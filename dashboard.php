015194efd295ac6d55fc8bf8565a744288c576a7
yuet25chan <chanyuet1993@gmail.com>
2025-08-22 15:12:15 -0400

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -15,37 +15,7 @@ require_once "navbar.php";
 
 ?>
 
-<!--
- <nav class="navbar navbar-expand-lg navbar-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="./">Task Master</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./about.php">About Us</a>
-                        </li>
-                       <li class="nav-item">
-                            <a class="nav-link" href="./contactus.php">Contact Us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./logout.php">Log Out</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        
 
-
-<div class="container"><div class="row"><div class="col-md-12">
-
--->
 <main class="container">
 <h1>Dashboard</h1>
 <h2>Welcome, <?= htmlspecialchars($_SESSION['username']); ?>!</h2>
